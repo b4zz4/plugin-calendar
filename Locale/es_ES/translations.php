25a2005e3fce122d4e0954c221c6b6aa3e176429
Bazza <ernesto_bazzano@yahoo.com.ar>
2023-03-02 23:25:27 -0300

    Update translations.php

--- Locale/es_ES/translations.php
+++ Locale/es_ES/translations.php
@@ -2,12 +2,11 @@
 
 return array(
     'Calendar' => 'Calendario',
-    // 'Calendar settings' => '',
-    // 'Project calendar view' => '',
-    // 'User calendar view' => '',
-    // 'My calendar' => '',
-    // 'Switch to the list view' => '',
-    // 'Show tasks based on the start date' => '',
-    // 'Subtasks time tracking' => '',
+    'Calendar settings' => 'Preferencias del calendario',
+    'Project calendar view' => 'Vista del calendario del proyecto',
+    'User calendar view' => 'Calendario de usuario(a)',
+    'My calendar' => 'Mi calendario',
+    'Switch to the list view' => 'Cambiar a la vista de la lista',
+    'Show tasks based on the start date' => 'Mostrar tareas basadas en la fecha de inicio',
+    'Subtasks time tracking' => 'Tiempo de subtarea',
 );
-
